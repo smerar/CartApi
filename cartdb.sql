43a9e19dc3bc21391a73249b75fafa8cfddecdcb
smerar <smera.professional3@gmail.com>
2024-03-08 23:53:58 -0500

    Commit - Database created

--- cartdb.sql
+++ cartdb.sql
@@ -1,7 +1,22 @@
-# ECommerceAPI
-PHP API serving MySQL data to ReactJS client. Handles Products, Users, Comments, Cart, and Orders. Tested with Postman. Clean, secure codebase.
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 09, 2024 at 04:51 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
---Database Schema
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `cartdb`
 --
