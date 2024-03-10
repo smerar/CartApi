99b96925f650f6f0590f8f17febc94de2a31ce60
smerar <smera.professional3@gmail.com>
2024-03-10 18:02:16 -0400

    Commit - DisplayAllProductsApi

--- api/product/displayAllProductsApi.php
+++ api/product/displayAllProductsApi.php
@@ -1,7 +1,34 @@
 <?php
 
-include('../../include/headersForGet.php.php');
-require '../product/displayAllProductsApi.php';
-include('../../Models/products.php');
+// localhost/CARTAPI/product/api/product/displayAllProductsApi.php
+
+header('Access-Control-Allow-Origin:*');
+header('Content-Type:application/json');
+header('Access-Control-Allow-Method: GET');
+header('Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Headers,Authorization,X-Request-With');
+
+include('productController.php');
+
+$requestMethod=$_SERVER["REQUEST_METHOD"];
+
+if($requestMethod=='GET')   
+{
+    $response=displayAllProducts();
+    echo $response;
+}
+else
+{
+    $data=[
+        'status'=>405,
+        'message'=>$requestMethod.' Method Not Allowed',
+    ];
+    header("HTTP/1.0 Method Not Allowed");
+    echo json_encode($data);
+}
+
+
+
+
+
 
 ?>
